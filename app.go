bb2ce46ce7b4729f1dbbc2532dd240cdea667d37
huylq <lequochuy930@gmail.com>
2021-01-30 13:33:39 +0700

    init

---
+++ app.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"boilerplate/database"
+	"boilerplate/handlers"
+
+	"flag"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+var (
+	port = flag.String("port", ":8080", "Port to listen on")
+)
+
+func main() {
+	// Connected with database
+	database.Connect()
+
+	// Create fiber app
+	app := fiber.New(fiber.Config{})
+
+	// Middleware
+	app.Use(logger.New())
+
+	// Create a /api/v1 endpoint
+	v1 := app.Group("/api/v1")
+
+	// Bind handlers
+	v1.Get("/users", handlers.UserList)
+	v1.Post("/users", handlers.UserCreate)
+
+	// Setup static files
+	app.Get("/", handlers.HealthCheck)
+	log.Fatal(app.Listen(*port))
+}
